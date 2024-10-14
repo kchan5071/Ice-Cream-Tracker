8103f5319b84170e338d06addeba983f553df856
Giovanna Ehrig <74570609+uhohgio@users.noreply.github.com>
2024-10-13 18:00:48 -0700

    Updating the website files

---
+++ websitefiles/helpButton.js
@@ -0,0 +1,68 @@
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("help");
+// var span = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// Function to handle showing modals
+function showModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+// Function to handle closing modals
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// Event listeners for each help button and close button
+document.getElementById("help-shipping").onclick = function() {
+    showModal("myModal-shipping");
+};
+document.getElementById("help-billing").onclick = function() {
+    showModal("myModal-billing");
+};
+document.getElementById("help-description").onclick = function() {
+    showModal("myModal-description");
+};
+document.getElementById("help-orderDate").onclick = function() {
+    showModal("myModal-orderDate");
+};
+document.getElementById("help-paymentDate").onclick = function() {
+    showModal("myModal-paymentDate");
+};
+document.getElementById("help-customerStatus").onclick = function() {
+    showModal("myModal-customerStatus");
+};
+
+// Event listeners for closing modals
+document.querySelectorAll('.close').forEach(function(element) {
+    element.onclick = function() {
+        var modalId = this.getAttribute('data-modal');
+        closeModal(modalId);
+    };
+});
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+};
+
--- websitefiles/icetrackJS.js
+++ websitefiles/icetrackJS.js
@@ -37,3 +37,61 @@ const textarea = document.getElementById('orderDescription');
     // Optionally, enable the submit button if you want to allow submission after selection
     document.getElementById('submitBtn').disabled = false; 
 }
+
+// Function to handle showing modals
+function showModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+// Function to handle closing modals
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// Event listeners for each help button and close button
+document.getElementById("help-shipping").onclick = function() {
+    showModal("myModal-shipping");
+};
+document.getElementById("help-billing").onclick = function() {
+    showModal("myModal-billing");
+};
+
+// Event listeners for closing modals
+document.querySelectorAll('.close').forEach(function(element) {
+    element.onclick = function() {
+        var modalId = this.getAttribute('data-modal');
+        closeModal(modalId);
+    };
+});
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+};
+
+
+// // the following controls the help button on-click pop-up
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myButton");
+// var span = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
